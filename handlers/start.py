a552a5a53570fed4204d5c3d76f3ee939de03fe9
TomyPrs <tomy.prs25@gmail.com>
2021-03-10 11:14:25 +0700

    stop start private
    Signed-off-by: TomyPrs <tomy.prs25@gmail.com>

--- handlers/start.py
+++ handlers/start.py
@@ -2,11 +2,11 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(
-    filters.command("start")
-    & filters.private
-    & ~ filters.edited
-)
+# @Client.on_message(
+#    filters.command("start")
+#    & filters.private
+#    & ~ filters.edited
+# )
 async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
